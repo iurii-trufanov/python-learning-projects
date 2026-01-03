cc3feec034329c9d6906779498c26bcde5422eca
iurii-trufanov <250752068+iurii-trufanov@users.noreply.github.com>
2026-01-03 03:23:27 +0300

    Add Hello_World_Simple Python project

---
+++ Hello_World_Simple/Hello_World_Simple.py
@@ -0,0 +1,13 @@
+
+"""
+Project: Hello World Simple - My First Python Program
+Author: Iurii Trufanov
+Date: December 2025
+Description: Simple program that prints greetings
+How to run: python Hello_World_Simple.py
+"""
+# Hello World Simple
+print("Hello, World!")
+print("Привет, Мир!")
+print("Hello, Андрей!")
+print("Hello, John!")
